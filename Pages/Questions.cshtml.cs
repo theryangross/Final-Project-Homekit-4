24ebbafa0763f150238ef3d3f69c89ab62365892
Ryan Gross <ryangross123@icloud.com>
2019-05-08 20:23:19 -0500

    Added questions page

---
+++ Pages/Questions.cshtml.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Final_Project_Homekit_4.Models;
+
+
+
+namespace Final_Project_Homekit_4.Pages
+{
+    public class ContactModel : PageModel
+    {
+        private readonly ILogger _log;
+
+        [BindProperty]
+        [Display(Name = "First Name")]
+        [Required]
+        public string FirstName {get; set;}
+
+        [BindProperty]
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName {get; set;}
+
+        [BindProperty]
+        [Required]
+        [Display(Name = "E-Mail")]
+        [EmailAddress(ErrorMessage = "The E-Mail field is not valid.")]
+        public string Email {get; set;}
+
+        [BindProperty]
+        [Required]
+        [Display(Name = "Question")]
+        public string Question {get; set;}
+
+        public ContactModel(ILogger<ContactModel> log)
+        {
+            _log = log;
+        }
+
+         public void OnPost()
+        {
+          _log.LogInformation($"IndexModel OnGet( {FirstName} ) Called",FirstName);
+        }
+
+        public void OnGet()
+        {
+            
+        }
+
+       
+    }
+}
